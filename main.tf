825aba5efffb05f0c2c55928baf2f585df4dfa1e
Lonnie Coffman <lonniecoffman@icloud.com>
2023-01-01 06:43:24 -0600

    igw and rt

--- main.tf
+++ main.tf
@@ -3,41 +3,48 @@ provider "aws" {
   profile = "iamadmin-general"
 }
 
-variable "availability_zone" {
-  description = "availability zone"
-}
-variable "cidr_blocks" {
-  description = "cidr blocks"
-  type        = list(object({ cidr_block = string, name = string }))
-}
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "availability_zone" {}
+variable "env_prefix" {}
 
 resource "aws_vpc" "development-vpc" {
-  cidr_block = var.cidr_blocks[0].cidr_block
+  cidr_block = var.vpc_cidr_block
   tags = {
-    Name : var.cidr_blocks[0].cidr_block
+    Name : "${var.env_prefix}-vpc"
   }
 }
 
 resource "aws_subnet" "dev-subnet-1" {
   vpc_id            = aws_vpc.development-vpc.id
-  cidr_block        = var.cidr_blocks[1].cidr_block
+  cidr_block        = var.subnet_cidr_block
   availability_zone = var.availability_zone
   tags = {
-    Name : var.cidr_blocks[1].name
+    Name : "${var.env_prefix}-subnet-1"
   }
 }
 
-data "aws_vpc" "existing_vpc" {
-  default = true
+resource "aws_internet_gateway" "development-igw" {
+  vpc_id = aws_vpc.development-vpc.id
+  tags = {
+    Name : "${var.env_prefix}-igw"
+  }
 }
 
-resource "aws_subnet" "dev-subnet-2" {
-  vpc_id            = data.aws_vpc.existing_vpc.id
-  cidr_block        = var.cidr_blocks[2].cidr_block
-  availability_zone = var.availability_zone
-  tags = {
-    Name = var.cidr_blocks[2].name
+resource "aws_route_table" "development-route-table" {
+  vpc_id = aws_vpc.development-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.development-igw.id
   }
+  tags = {
+    Name : "${var.env_prefix}-rtb"
+  }
+}
+
+resource "aws_route_table_association" "a-rtb-subnet" {
+  subnet_id      = aws_subnet.dev-subnet-1.id
+  route_table_id = aws_route_table.development-route-table.id
 }
 
 output "dev-vpc-id" {
